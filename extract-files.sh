c191e922f3fdebbb13c0797384711ee1400bbddf
johnmart19 <johnivan19999@gmail.com>
2022-11-08 03:55:31 +0200

    sm8250-common: Import and adapt Xiaomi Proprietary vibrator hal
    This change includes:
    - Sepolicy ports from Miui for Xiaomi Vibrator and VibratorFeature hals
    - Import needed Prebuilts of AIDL service and required deps
    - Copy vndk 32 libutils and patch service with it to fix bootloop issues
    
    This change requires:
    - Import Stock vibator hal effects from Miui vendor in device specific trees

--- extract-files.sh
+++ extract-files.sh
@@ -75,8 +75,10 @@ function blob_fixup() {
         vendor/etc/media_codecs_kona.xml)
             sed -i "/media_codecs_dolby_audio.xml/d" "${2}"
             ;;
-   esac
-}
+        vendor/bin/hw/vendor.xiaomi.hardware.vibratorfeature.service)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            ;;
+    esac
 
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
